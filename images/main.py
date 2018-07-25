bd42f13537b920c8964229236917108efd37acf7
Tim Blasi <tjblasi@google.com>
2018-07-24 17:19:49 -0700

    Added some comments to explain what is happening

--- images/main.py
+++ images/main.py
@@ -1,6 +1,8 @@
 import webapp2
 import os
 import jinja2
+# You need base64 to send the image to HTML as a string the browser can
+# understand
 import base64
 from google.appengine.api import users
 from google.appengine.ext import ndb
@@ -12,29 +14,35 @@ jinja_current_dir = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
+# Gets a key for the provided logged-in user.
+def get_key(me):
+    return ndb.Key('Visitor', me.user_id())
+
+# Returns the Visitor object associated with the logged-in user.
+# If there is no user logged in, returns None.
 def get_visitor():
     me = users.get_current_user()
     if not me:
         return None
-    my_key = ndb.Key('Visitor', me.user_id())
+    my_key = get_key(me)
     return my_key.get()
 
 class SigninHandler(webapp2.RequestHandler):
     def get(self):
         me = users.get_current_user()
         if not me:
+            # No user is logged in, send them to the welcome page.
             start_template = jinja_current_dir.get_template("templates/welcome.html")
             jinja_values = {
                 'signin_page_url': users.create_login_url('/')
             }
-
             self.response.write(start_template.render(jinja_values))
         else:
-            my_key = ndb.Key('Visitor', me.user_id())
+            # A user is logged in
             my_visitor = get_visitor()
             if not my_visitor:
                 my_visitor = Visitor(
-                    key = my_key,
+                    key = get_key(me),
                     id = me.user_id(),
                     name = me.nickname(),
                     email = me.email(),
@@ -49,6 +57,10 @@ class SigninHandler(webapp2.RequestHandler):
                 'signout_page_url': users.create_logout_url('/'),
             }
             if my_visitor.image:
+                # base64.b64encode converts the BlobProperty image
+                # into a string the browser knows how to show as an image.
+                # See withuser.html for the <img> code needed to show a string
+                # as an image.
                 jinja_values['img'] = base64.b64encode(my_visitor.image)
 
             self.response.write(withuser_template.render(jinja_values))
--- images/models.py
+++ images/models.py
@@ -5,4 +5,5 @@ class Visitor(ndb.Model):
     name =  ndb.StringProperty(required=True)
     email =  ndb.StringProperty(required=True)
     id =  ndb.StringProperty(required=True)
+    # Images can only be stored as "BlobProperty"
     image = ndb.BlobProperty()
